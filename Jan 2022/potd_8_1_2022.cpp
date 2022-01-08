61a2afc570e5fd0a0a5265c72a6bcc1b5958c1ac
Kshitij Dwivedi <kshitijdwivedi28@gmail.com>
2022-01-08 14:17:43 +0530

    Update potd_8_1_2022.cpp

--- Jan 2022/potd_8_1_2022.cpp
+++ Jan 2022/potd_8_1_2022.cpp
@@ -1,40 +1,40 @@
-N Digit numbers with digits in increasing order 
-Medium Accuracy: 78.69% Submissions: 2064 Points: 4
-Given an integer N, print all the N digit numbers in increasing order, such that their digits are in strictly increasing order(from left to right).
+// N Digit numbers with digits in increasing order 
+// Medium Accuracy: 78.69% Submissions: 2064 Points: 4
+// Given an integer N, print all the N digit numbers in increasing order, such that their digits are in strictly increasing order(from left to right).
 
-Example 1:
+// Example 1:
 
-Input:
-N = 1
-Output:
-0 1 2 3 4 5 6 7 8 9
-Explanation:
-Single digit numbers are considered to be 
-strictly increasing order.
-Example 2:
+// Input:
+// N = 1
+// Output:
+// 0 1 2 3 4 5 6 7 8 9
+// Explanation:
+// Single digit numbers are considered to be 
+// strictly increasing order.
+// Example 2:
 
-Input:
-N = 2
-Output:
-12 13 14 15 16 17 18 19 23....79 89
-Explanation:
-For N = 2, the correct sequence is
-12 13 14 15 16 17 18 19 23 and so on 
-up to 89.
-Your Task:  
-You don't need to read input or print anything. Your task is to complete the function increasingNumbers() which takes an integer N as an input parameter and return the list of numbers such that their digits are in strictly increasing order.
+// Input:
+// N = 2
+// Output:
+// 12 13 14 15 16 17 18 19 23....79 89
+// Explanation:
+// For N = 2, the correct sequence is
+// 12 13 14 15 16 17 18 19 23 and so on 
+// up to 89.
+// Your Task:  
+// You don't need to read input or print anything. Your task is to complete the function increasingNumbers() which takes an integer N as an input parameter and return the list of numbers such that their digits are in strictly increasing order.
 
-Expected Time Complexity: O(N)
-Expected Auxiliary Space: O(N)
+// Expected Time Complexity: O(N)
+// Expected Auxiliary Space: O(N)
 
-Constraints:
-1 <= N <= 9
+// Constraints:
+// 1 <= N <= 9
 
-View Bookmarked Problems
-Company Tags
- ABCO
-Topic Tags
- Recursion
+// View Bookmarked Problems
+// Company Tags
+//  ABCO
+// Topic Tags
+//  Recursion
  
  
  // { Driver Code Starts
