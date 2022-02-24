029a717c5c96efaa2d42ebe7c64b04b91a8e8862
Kshitij Dwivedi <kshitijdwivedi28@gmail.com>
2022-02-24 12:15:29 +0530

    Create Return Two Prime Numbers.cpp

---
+++ Feb 2022/Return Two Prime Numbers.cpp
@@ -0,0 +1,91 @@
+// Return two prime numbers 
+// Hard Accuracy: 78.46% Submissions: 4547 Points: 8
+// Given an even number N (greater than 2), return two prime numbers whose sum will be equal to given number. There are several combinations possible. Print only the pair whose minimum value is the smallest among all the minimum values of pairs and print the minimum element first.
+
+// NOTE: A solution will always exist, read Goldbachs conjecture. 
+
+// Example 1:
+
+// Input: N = 74
+// Output: 3 71
+// Explaination: There are several possibilities 
+// like 37 37. But the minimum value of this pair 
+// is 3 which is smallest among all possible 
+// minimum values of all the pairs.
+// Example 2:
+
+// Input: 4
+// Output: 2 2
+// Explaination: This is the only possible 
+// prtitioning of 4.
+// Your Task:
+// You do not need to read input or print anything. Your task is to complete the function primeDivision() which takes N as input parameter and returns the partition satisfying the condition.
+
+// Expected Time Complexity: O(N*log(logN))
+// Expected Auxiliary Space: O(N)
+
+// Constraints:
+// 1 ≤ N ≤ 104  
+
+// View Bookmarked Problems
+// Company Tags
+//  Yahoo Zoho
+// Topic Tags
+//  Mathematical
+// Related Interview Experiences
+//  Zoho interview experience set 21 campus
+// We are replacing the old Disqus forum with the new Discussions section given below.
+// Click here to view old Disqus comments.
+
+
+
+
+
+
+
+
+
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<int> primeDivision(int N){
+        // code here
+        vector<int> prime_seive(N+1, 0);
+
+        for(int i = 2; i <= N; i++)
+            if(prime_seive[i] == 0)
+                for(int j = 2*i; j < N+1; j += i)
+                    prime_seive[j] = 1;
+
+        for(int i = 2; i < N+1; i++)
+            if (prime_seive[i] == 0 && prime_seive[N-i] == 0)
+                return {i, (N-i)};
+        
+        return {};
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        vector<int> ans = ob.primeDivision(N);
+        cout<<ans[0]<<" "<<ans[1]<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
