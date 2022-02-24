046c4f3527c316f49f3b16bb6d54943d083ce5ab
Kshitij Dwivedi <kshitijdwivedi28@gmail.com>
2022-02-24 12:17:06 +0530

    Update Return Two Prime Numbers.cpp

--- Feb 2022/Return Two Prime Numbers.cpp
+++ Feb 2022/Return Two Prime Numbers.cpp
@@ -40,6 +40,81 @@
 
 
 
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<int> primeDivision(int N){
+        // code here
+        vector<int> prime_seive(N+1, 0);
+
+        for(int i = 2; i <= N; i++)
+        {
+            if(prime_seive[i] == 0)
+            {
+                for(int j = 2*i; j < N+1; j += i)
+                    prime_seive[j] = 1;
+            }
+        }
+
+        vector<int> prime;
+        for(int i = 2; i < N+1; i++)
+            if(prime_seive[i] == 0)
+                prime.push_back(i);
+
+
+        
+        unordered_map<int, int> m;
+        for(auto value : prime)
+            m[value]++;
+            
+        vector<pair<int, int>> vp;
+        
+        for(auto value : prime)
+            if (m[N-value])
+                vp.push_back({value, (N-value)});
+
+        
+        sort(vp.begin(), vp.end());
+        
+        return {vp[0].first, vp[0].second};
+
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        vector<int> ans = ob.primeDivision(N);
+        cout<<ans[0]<<" "<<ans[1]<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
+
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
 
 
 
